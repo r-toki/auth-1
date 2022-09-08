001f51a4ccc8fa22f698beef1d1809bf2c328e86
rtokkie <natto.and.eggs@gmail.com>
2022-09-09 08:13:47 +0900

    b

--- src/lib/password_hashing.rs
+++ src/lib/password_hashing.rs
@@ -1,3 +1,21 @@
-pub fn hash() {}
+use anyhow::Result;
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
 
-pub fn verify() {}
+pub fn hash(password: String) -> Result<String, argon2::password_hash::Error> {
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string();
+    Ok(hashed_password)
+}
+
+pub fn verify(
+    password: String,
+    hashed_password: String,
+) -> Result<(), argon2::password_hash::Error> {
+    let hashed_password = PasswordHash::new(&hashed_password)?;
+    Argon2::default().verify_password(password.as_bytes(), &hashed_password)
+}
