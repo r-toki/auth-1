b2556a6ada9ab864e18060c57a9f3339e1758bfb
rtokkie <natto.and.eggs@gmail.com>
2022-09-10 08:45:13 +0900

    d

---
+++ src/lib/jwt_extractor.rs
@@ -0,0 +1,85 @@
+use crate::lib::jwt::Claims;
+use crate::lib::jwt::{decode_access_token, decode_refresh_token};
+use actix_web::{http::header, FromRequest};
+use anyhow::Result;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::future::Future;
+use std::pin::Pin;
+
+lazy_static! {
+    static ref BEARER_REGEXP: Regex = Regex::new(r"^Bearer\s(.*)$").unwrap();
+}
+
+pub struct BearerToken(Option<String>);
+
+impl FromRequest for BearerToken {
+    type Error = actix_web::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    fn from_request(req: &actix_web::HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
+        let req = req.clone();
+        Box::pin(async move {
+            let token = extract_token(&req);
+            Ok(BearerToken(token))
+        })
+    }
+}
+
+pub struct AccessTokenDecoded(Option<Claims>);
+
+impl FromRequest for AccessTokenDecoded {
+    type Error = actix_web::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    fn from_request(req: &actix_web::HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
+        let req = req.clone();
+        Box::pin(async move {
+            match extract_token(&req) {
+                Some(token) => {
+                    let claims = decode_access_token(&token);
+                    match claims {
+                        Ok(claims) => Ok(AccessTokenDecoded(Some(claims))),
+                        Err(_) => Ok(AccessTokenDecoded(None)),
+                    }
+                }
+                None => Ok(AccessTokenDecoded(None)),
+            }
+        })
+    }
+}
+
+pub struct RefreshTokenDecoded(Option<Claims>);
+
+impl FromRequest for RefreshTokenDecoded {
+    type Error = actix_web::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    fn from_request(req: &actix_web::HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
+        let req = req.clone();
+        Box::pin(async move {
+            match extract_token(&req) {
+                Some(token) => {
+                    let claims = decode_refresh_token(&token);
+                    match claims {
+                        Ok(claims) => Ok(RefreshTokenDecoded(Some(claims))),
+                        Err(_) => Ok(RefreshTokenDecoded(None)),
+                    }
+                }
+                None => Ok(RefreshTokenDecoded(None)),
+            }
+        })
+    }
+}
+
+fn extract_token(req: &actix_web::HttpRequest) -> Option<String> {
+    req.headers()
+        .get(header::AUTHORIZATION)
+        .and_then(|v| v.to_str().ok())
+        .and_then(|authorization| {
+            BEARER_REGEXP
+                .captures(authorization)
+                .and_then(|captures| captures.get(1))
+        })
+        .map(|v| v.as_str().to_owned())
+}
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,3 +1,4 @@
 pub mod config;
 pub mod jwt;
+pub mod jwt_extractor;
 pub mod password_hashing;
