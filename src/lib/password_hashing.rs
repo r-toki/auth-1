e65bca9318b012e3a5de9341981d7fc4cf2883af
rtokkie <natto.and.eggs@gmail.com>
2022-09-11 13:48:48 +0900

    m

--- src/lib/password_hashing.rs
+++ src/lib/password_hashing.rs
@@ -1,10 +1,9 @@
-use anyhow::Result;
 use argon2::{
     password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Argon2,
 };
 
-pub fn hash(password: &str) -> Result<String, argon2::password_hash::Error> {
+pub fn hash(password: &str) -> anyhow::Result<String, argon2::password_hash::Error> {
     let salt = SaltString::generate(&mut OsRng);
     let hashed_password = Argon2::default()
         .hash_password(password.as_bytes(), &salt)?
